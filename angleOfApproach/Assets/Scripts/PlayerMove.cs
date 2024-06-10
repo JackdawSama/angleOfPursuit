5101d337d5c42891dc0b8903f275d14c1438beac
Akshay Bharadhwaj <akshaybharadhwaj98@gmail.com>
2024-06-10 18:54:44 -0400

    Added player input

--- angleOfApproach/Assets/Scripts/PlayerMove.cs
+++ angleOfApproach/Assets/Scripts/PlayerMove.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 _input;
+    private Vector3 _direction;
+    private CharacterController _controller;
+
+    [SerializeField] private float _speed;
+
+    void Awake()
     {
-        
+        _controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _controller.Move(_direction * _speed * Time.deltaTime);
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+
+        _direction = new Vector3(_input.x, 0, _input.y);
+        Debug.Log(_input);
     }
 }
