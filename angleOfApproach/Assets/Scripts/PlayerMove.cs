037c091aac76efe532183c79699cd44355f79ac3
Akshay Bharadhwaj <akshaybharadhwaj98@gmail.com>
2024-06-10 20:49:41 -0400

    Updated camera

--- angleOfApproach/Assets/Scripts/PlayerMove.cs
+++ angleOfApproach/Assets/Scripts/PlayerMove.cs
@@ -10,7 +10,11 @@ public class PlayerMove : MonoBehaviour
     private Vector3 _direction;
     private CharacterController _controller;
 
+    private float _currentVelocity;
+
     [SerializeField] private float _speed;
+    [SerializeField] private float _turnSmoothing;
+
 
     void Awake()
     {
@@ -19,13 +23,23 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        _controller.Move(_direction * _speed * Time.deltaTime);
+        Move();
     }
-    public void Move(InputAction.CallbackContext context)
+
+    public void GetInput(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<Vector2>();
-
         _direction = new Vector3(_input.x, 0, _input.y);
-        Debug.Log(_input);
+    }
+
+    public void Move()
+    {
+        if(_input.sqrMagnitude == 0) return;
+
+        var targetAngle = Mathf.Atan2(_input.x, _input.y) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, _turnSmoothing);
+        transform.rotation = Quaternion.Euler(0, angle, 0);
+
+        _controller.Move(_direction * _speed * Time.deltaTime);
     }
 }
