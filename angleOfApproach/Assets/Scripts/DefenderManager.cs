b5bb4c73ff7540ab25ebdd53c8a28041d83aba7f
Akshay Bharadhwaj <akshaybharadhwaj98@gmail.com>
2024-06-12 00:14:57 -0400

    Removed unnecessary scripts

--- angleOfApproach/Assets/Scripts/DefenderManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DefenderManager : MonoBehaviour
-{
-    [SerializeField] private Defender[] defendersList;
-
-}
